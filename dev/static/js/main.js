ad91b69912e052647202a4c0d0edd27ba06bed12
esmol <smol18@bk.ru>
2020-01-24 17:58:08 +0300

    header

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,6 +1,84 @@
 $(document).ready(function () {
-    svg4everybody({});
+
+    $(function () {
+        $(document).on("click", ".mobile_menu_container .parent", function (e) {
+            e.preventDefault();
+            $(this).siblings("ul").addClass("loaded");
+        });
+        $(document).on("click", ".mobile_menu_container .back", function (e) {
+            e.preventDefault();
+            $(this).parent().parent().removeClass("loaded");
+        });
+        $(document).on("click", ".sandwich", function (e) {
+            e.preventDefault();
+            $(".mobile_menu_container").addClass("loaded");
+            $(".mobile_menu_overlay").fadeIn();
+        });
+        $(document).on("click", ".mobile_menu_overlay", function (e) {
+            $(".mobile_menu_container").removeClass("loaded");
+            $(this).fadeOut();
+        });
+    })
+
+    $(".navbar").on("click", "a", function (event) {
+        // исключаем стандартную реакцию браузера
+        event.preventDefault();
+
+        // получем идентификатор блока из атрибута href
+        var id = $(this).attr('href'),
+
+            // находим высоту, на которой расположен блок
+            top = $(id).offset().top;
+
+        // анимируем переход к блоку, время: 800 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 500);
+    });
+
+
+    $(window).scroll(function () {
+        var $sections = $('section');
+        $sections.each(function (i, el) {
+            var top = $(el).offset().top - 100;
+            var bottom = top + $(el).height();
+            var scroll = $(window).scrollTop();
+            var id = $(el).attr('id');
+            if (scroll > top && scroll < bottom) {
+                $('a.active').removeClass('active');
+                $('.div.active').removeClass('active');
+                $('.div[href="#' + id + '"]').addClass('active');
+                $('a[href="#' + id + '"]').addClass('active');
+            }
+        })
+        if ($(this).scrollTop() > '500') {
+            $('.navbar').addClass('fixed');
+        } else if ($(this).scrollTop() < 500) {
+            $('.navbar').removeClass('fixed');
+        }
+    });
+
+    var btn = $('.top');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 500) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+
 });
+
+
+
 
 
 // Полифилы
